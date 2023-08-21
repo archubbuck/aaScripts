f313fcfef5b9c8ac85f1b3d2a4de708e1a4be525
Adam Chubbuck <adam.chubbuck@gmail.com>
2023-08-20 21:18:02 -0400

    up

--- src/main/java/org/aa/bloodyhell/tasks/navigation/GoToAltar.java
+++ src/main/java/org/aa/bloodyhell/tasks/navigation/GoToAltar.java
@@ -40,7 +40,6 @@ public class GoToAltar implements Task {
         }
 
         if (atArea(Areas.CAVE_1_AREA)) {
-//            Tile tile = new Tile(3447, 9822);
             Tile tile = new Tile(3447, 9824);
             GameObject caveEntrance = Objects.stream().name("Cave entrance").within(tile, 3).first();
             return caveEntrance.valid() && caveEntrance.interact("Enter") && Condition.wait(() -> atArea(Areas.CAVE_2_AREA), 150, 10);
@@ -52,8 +51,12 @@ public class GoToAltar implements Task {
             return caveEntrance.valid() && caveEntrance.interact("Enter") && Condition.wait(() -> atArea(Areas.CAVE_3_AREA), 150, 10);
         }
 
+        Area shortcutArea = new Area(new Tile(3534, 9775, 0), new Tile(3539, 9765, 0));
+
         if (atArea(Areas.CAVE_3_AREA)) {
-            Tile tile = new Tile(3500, 9804);
+            Tile tile = Skills.level(Skill.Agility) >= 93 ? new Tile(3500, 9804) : new Tile(3483, 9832);
+            String caveObjName = Skills.level(Skill.Agility) >= 93 ? "Cave" : "Cave entrance";
+            Area targetArea = Skills.level(Skill.Agility) >= 93 ? shortcutArea : Areas.CAVE_4_AREA;
             if (tile.distance() > 5) {
                 boolean navigated = Movement.builder(tile)
                         .setRunMin(15)
@@ -63,16 +66,67 @@ public class GoToAltar implements Task {
                         .getSuccess();
                 if (!navigated) return false;
             }
-            GameObject caveEntrance = Objects.stream().name("Cave").within(tile, 1).first();
-            return caveEntrance.valid() && caveEntrance.interact("Enter") && Condition.wait(() -> atArea(Areas.CAVE_4_AREA), 150, 10);
+            GameObject caveEntrance = Objects.stream().name(caveObjName).within(tile, 1).first();
+            return caveEntrance.valid() && caveEntrance.interact("Enter") && Condition.wait(() -> atArea(targetArea), 150, 10);
         }
-
-        Area shortcutArea = new Area(new Tile(3534, 9775, 0), new Tile(3539, 9765, 0));
 
         if (atArea(shortcutArea)) {
             Tile tile = new Tile(3539, 9772);
             GameObject caveEntrance = Objects.stream().name("Cave").within(tile, 1).first();
             return caveEntrance.valid() && caveEntrance.interact("Enter") && Condition.wait(() -> atArea(Areas.CAVE_7_AREA), 150, 10);
+        }
+
+        if (atArea(Areas.CAVE_4_AREA)) {
+            Tile tile = new Tile(3492, 9862);
+            if (tile.distance() > 5) {
+                boolean navigated = Movement.builder(tile)
+                        .setRunMin(15)
+                        .setRunMax(75)
+                        .setUseTeleports(false)
+                        .move()
+                        .getSuccess();
+                if (!navigated) return false;
+            }
+            GameObject caveEntrance = Objects.stream().name("Cave entrance").within(tile, 1).first();
+            return caveEntrance.valid()
+                    && caveEntrance.interact("Enter")
+                    && Condition.wait(() -> atArea(Areas.CAVE_5_AREA), 125, 25);
+        }
+
+        if (atArea(Areas.CAVE_5_AREA)) {
+            GameObject caveEntrance = Objects.stream().name("Cave").within(Tiles.CAVE_5_EXIT, 1).first();
+
+            if (!caveEntrance.inViewport(true)) {
+                boolean navigated = Movement.builder(Tiles.CAVE_5_EXIT)
+                        .setRunMin(15)
+                        .setRunMax(75)
+                        .setUseTeleports(false)
+                        .move()
+                        .getSuccess();
+                if (!navigated) return false;
+            }
+
+            return caveEntrance.valid()
+                    && caveEntrance.interact("Enter")
+                    && Condition.wait(() -> atArea(Areas.CAVE_6_AREA), 125, 25);
+        }
+
+        if (atArea(Areas.CAVE_6_AREA)) {
+            GameObject caveEntrance = Objects.stream().name("Cave").within(Tiles.CAVE_6_EXIT, 1).first();
+
+            if (!caveEntrance.inViewport(true)) {
+                boolean navigated = Movement.builder(Tiles.CAVE_6_EXIT)
+                        .setRunMin(15)
+                        .setRunMax(75)
+                        .setUseTeleports(false)
+                        .move()
+                        .getSuccess();
+                if (!navigated) return false;
+            }
+
+            return caveEntrance.valid()
+                    && caveEntrance.interact("Enter")
+                    && Condition.wait(() -> atArea(Areas.CAVE_7_AREA), 125, 25);
         }
 
         if (atArea(Areas.CAVE_7_AREA)) {
@@ -94,83 +148,6 @@ public class GoToAltar implements Task {
                     && mysteriousRuins.interact("Enter")
                     && Condition.wait(() -> atArea(Areas.BLOOD_ALTAR_AREA), 150, 10);
         }
-
-//        if (atArea(Areas.EDGEVILLE_AREA) || atArea(Areas.CAVE_1_AREA)) {
-//            return navigateTo(Tiles.CAVE_2_ENTER);
-//        }
-//
-//        if (atArea(Areas.CAVE_2_AREA)) {
-//            return navigateTo(Tiles.CAVE_3_ENTER);
-//        }
-//
-//        if (atArea(Areas.CAVE_3_AREA)) {
-//            return navigateTo(new Tile(3536, 9768));
-//        }
-//
-//        if (Skills.level(Skill.Agility) >= 93) {
-//            if (atArea(new Area(new Tile(3534, 9775, 0), new Tile(3539, 9765, 0)))) {
-//                GameObject caveEntrance = Objects.stream().name("Cave").within(new Tile(3539, 9772), 1).first();
-//                return caveEntrance.valid() && caveEntrance.interact("Enter") && Condition.wait(() -> atArea(Areas.CAVE_7_AREA), 125, 25);
-//            }
-//        }
-//
-//        if (atArea(Areas.CAVE_4_AREA)) {
-//            return navigateTo(Tiles.CAVE_5_ENTER);
-//        }
-//
-//        if (atArea(Areas.CAVE_5_AREA)) {
-//            GameObject caveEntrance = Objects.stream().name("Cave").within(Tiles.CAVE_5_EXIT, 1).first();
-//
-//            if (!caveEntrance.inViewport(true)) {
-//                boolean navigated = Movement.builder(Tiles.CAVE_5_EXIT)
-//                        .setRunMin(15)
-//                        .setRunMax(75)
-//                        .setUseTeleports(false)
-//                        .move()
-//                        .getSuccess();
-//                if (!navigated) return false;
-//            }
-//
-//            return caveEntrance.valid()
-//                    && caveEntrance.interact("Enter")
-//                    && Condition.wait(() -> atArea(Areas.CAVE_6_AREA), 125, 25);
-//        }
-//
-//        if (atArea(Areas.CAVE_6_AREA)) {
-//            GameObject caveEntrance = Objects.stream().name("Cave").within(Tiles.CAVE_6_EXIT, 1).first();
-//
-//            if (!caveEntrance.inViewport(true)) {
-//                boolean navigated = Movement.builder(Tiles.CAVE_6_EXIT)
-//                        .setRunMin(15)
-//                        .setRunMax(75)
-//                        .setUseTeleports(false)
-//                        .move()
-//                        .getSuccess();
-//                if (!navigated) return false;
-//            }
-//
-//            return caveEntrance.valid()
-//                    && caveEntrance.interact("Enter")
-//                    && Condition.wait(() -> atArea(Areas.CAVE_7_AREA), 125, 25);
-//        }
-//
-//        if (atArea(Areas.CAVE_7_AREA)) {
-//            GameObject mysteriousRuins = Objects.stream().name("Mysterious ruins").action("Enter").first();
-//
-//            if (mysteriousRuins.valid() && mysteriousRuins.distance() > 5) {
-//                Area area = new Area(new Tile(3557, 9786, 0), new Tile(3565, 9780, 0));
-//                boolean navigated = Movement.builder(area.getRandomTile())
-//                        .setRunMin(15)
-//                        .setRunMax(75)
-//                        .setUseTeleports(false)
-//                        .move()
-//                        .getSuccess();
-//                if (!navigated) return false;
-//            }
-//            return mysteriousRuins.valid()
-//                    && mysteriousRuins.interact("Enter")
-//                    && Condition.wait(() -> atArea(Areas.BLOOD_ALTAR_AREA), 125, 25);
-//        }
 
         return false;
     }
